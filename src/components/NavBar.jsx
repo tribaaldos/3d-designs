51fed258523ab73c568568acc2eaf5012128fb1b
tribaaldos <tribaaldos@gmail.com>
2023-08-21 14:44:01 -0700

    updating navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,72 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../components/NavBar.css';
 
 export default function NavBar() {
-    const [dropDown, setDropDown] = useState(false);
-
-    const toggleDropDown = () => {
-        setDropDown(!dropDown);
-    };
-
     return (
-        <nav>
-            <ul>
-                <li className="dropdown">
-                    <button className="dropdown-btn" onClick={toggleDropDown}>3d Basics</button>
-                    {dropDown && (
-                        <ul className="dropdown-menu">                <li><Link to="/page1">Animations</Link></li>
-                        <li><Link to="/page2">Cameras</Link></li>
-                        <li><Link to="/page3">FullScreen</Link></li>
-                        <li><Link to="/page4">Geometry</Link></li>
-                        <li><Link to="/page5">Textures</Link></li>
-                        <li><Link to="/page6">Materials</Link></li>
-                        <li><Link to="/page7">3dText</Link></li>
-                        <li><Link to="/page8">TicTacToeGame</Link></li>
-                        </ul>
-                    )}
-                </li>
-            </ul>
+        <nav className="navbar">
+            <a href="#home">Home</a>
+
+            <div className="dropdown">
+                <button className="dropbtn">Dropdown</button>
+                <div className="dropdown-content">
+                    <Link to="/page1">Animations</Link>
+                    <Link to="/page2">Cameras</Link>
+                    <Link to="/page3">FullScreen</Link>
+                    <Link to="/page4">Geometry</Link>
+                    <Link to="/page5">Textures</Link>
+                    <Link to="/page6">Materials</Link>
+                    <Link to="/page7">3dText</Link>
+                    <Link to="/page8">TicTacToeGame</Link>
+                </div>
+            </div> 
+            <div className="dropdown">
+                <button className="dropbtn">Lesson 2</button>
+                <div className="dropdown-content">
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                </div>
+            </div>
+            <div className="dropdown">
+                <button className="dropbtn">Lesson 2</button>
+                <div className="dropdown-content">
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                </div>
+            </div>
+            <div className="dropdown">
+                <button className="dropbtn">Lesson 2</button>
+                <div className="dropdown-content">
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                </div>
+            </div>
+            <div className="dropdown">
+                <button className="dropbtn">Lesson 2</button>
+                <div className="dropdown-content">
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                    <Link to="">1</Link>
+                </div>
+            </div>
         </nav>
     );
 }
