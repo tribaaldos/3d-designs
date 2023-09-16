ec1ae12059efe6ed980fcd8f6887dfa7ab1c83af
tribaaldos <tribaaldos@gmail.com>
2023-09-16 18:20:20 +0200

    hey

--- src/pages/02/lights.jsx
+++ src/pages/02/lights.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import './styles.css'
 
 export default function Lights() {
     useEffect(() => {
